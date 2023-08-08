2069b85d795b98c474197a6c9f3a63a30d52b5b5
Devjallow <bjallow000@outlook.com>
2023-08-08 01:23:55 +0000

    added the logical calculation

--- Allocate.py
+++ Allocate.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 #money allocation program
+#money allocation program
+totalCash = int(input("Enter totalCash: "))
 Denomination = [200, 100, 50, 25, 20, 10, 5]
 count = 0
-for order in Denomination:
-    print("D{} * ".format(order),end="")
+for order in range(len(Denomination)):
+    print("D{} * ".format(Denomination[order]),end="")
     DenominationValue = int(input())
-    count += DenominationValue
-print(count)
+    totalAmount = Denomination[order] * DenominationValue
+    print(totalAmount)
+    count += totalAmount
+print("Total-Amount is: {}\n".format(count))
+if totalCash > count:
+    print("You have Surplus of {}".format(count - totalCash))
+elif totalCash == count:
+    print("Your money is OK {}".format(count))
+else:
+    print("You have a Shortage of {}".format(count - totalCash))
+
